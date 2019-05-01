be51ca8d1579b26f1d6de3bb3d2711b87031b121
Nicholas Lobo <niku01@gmail.com>
2019-05-01 17:56:13 -0300

    Resolved WIth and setting more objects

--- js/app.js
+++ js/app.js
@@ -1,28 +1,19 @@
-var ViewModel = function () {
-    this.clickCount = ko.observable(0);
-    this.name = ko.observable('Tabby');
-    this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
-    this.imgAttribution = ko.observable('https://live.staticflickr.com/4808/44430067970_45d8ea1a96_z.jpg')
-    this.level = ko.observable('baby');
-
-
+var Cat = function (data) {
+    this.clickCount = ko.observable(data.clickCount);
+    this.name = ko.observable(data.name);
+    this.imgSrc = ko.observable(data.imgSrc);
+    this.imgAttribution = ko.observable(data.imgAttribution);
+    this.level = ko.observable(data.nicknames);
 
     // Computed Observables
     this.fullName = ko.computed(function () {
-        return this.name() + " " + this.level();
-    }, this);
-
-    this.incrementCounter = function () {
-        // Set the ClickCount
-        // knockout will handle the view to model and movel to view synchronization
-        this.clickCount(this.clickCount() + 1);
-
         if (this.clickCount() >= 5 && this.clickCount() < 10) {
             this.level('teen');
         } else if (this.clickCount() >= 10) {
             this.level('dragon');
         }
-    };
+        return this.name() + " " + this.level();
+    }, this);
 
     // Control Flow - 1: Iterating over an array
     this.nicknames = ko.observableArray([{
@@ -32,7 +23,24 @@ var ViewModel = function () {
             nickname: 'tiger'
         }
     ]);
+};
 
-}
+
+var ViewModel = function () {
+
+    this.currentCat = ko.observable(new Cat({
+        clickCount: 0,
+        name: 'Tabby',
+        imgSrc: 'img/434164568_fea0ad4013_z.jpg',
+        imgAttribution: 'https://live.staticflickr.com/4808/44430067970_45d8ea1a96_z.jpg',
+        nicknames: ['fluffy', 'tiger']
+    }));
+
+    this.incrementCounter = function () {
+        // Set the ClickCount
+        // knockout will handle the view to model and movel to view synchronization
+        this.clickCount(this.clickCount() + 1);
+    };
+};
 
 ko.applyBindings(new ViewModel());
